5c8b95242cae5e2e37ef3a5ca409cc5d93a791df
Phil Brockman <phil.brockman@gmail.com>
2021-12-22 18:02:27 -0600

    fixed nav

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ class ScrollFrame(tk.Frame):
         super().__init__(parent) # create a frame (self)
 
         self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")          #place canvas on self
-        self.viewPort = tk.Frame(self.canvas, background="#ffffff")                    #place a frame on the canvas, this frame will hold the child widgets 
-        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview) #place a scrollbar on self 
+        self.viewPort = tk.Frame(self.canvas, background="#ffffff")                    #place a frame on the canvas, this frame will hold the child widgets
+        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview) #place a scrollbar on self
         self.canvas.configure(yscrollcommand=self.vsb.set)                          #attach scrollbar action to scroll of canvas
 
         self.vsb.pack(side="right", fill="y")                                       #pack scrollbar to right of self
@@ -20,13 +20,13 @@ class ScrollFrame(tk.Frame):
 
         self.viewPort.bind("<Configure>", self.onFrameConfigure)                       #bind an event whenever the size of the viewPort frame changes.
         self.canvas.bind("<Configure>", self.onCanvasConfigure)                       #bind an event whenever the size of the canvas frame changes.
-            
+
         self.viewPort.bind('<Enter>', self.onEnter)                                 # bind wheel events when the cursor enters the control
         self.viewPort.bind('<Leave>', self.onLeave)                                 # unbind wheel events when the cursorl leaves the control
 
         self.onFrameConfigure(None)                                                 #perform an initial stretch on render, otherwise the scroll region has a tiny border until the first resize
 
-    def onFrameConfigure(self, event):                                              
+    def onFrameConfigure(self, event):
         '''Reset the scroll region to encompass the inner frame'''
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))                 #whenever the size of the frame changes, alter the scroll region respectively.
 
@@ -45,7 +45,7 @@ class ScrollFrame(tk.Frame):
                 self.canvas.yview_scroll( -1, "units" )
             elif event.num == 5:
                 self.canvas.yview_scroll( 1, "units" )
-    
+
     def onEnter(self, event):                                                       # bind wheel events when the cursor enters the control
         if platform.system() == 'Linux':
             self.canvas.bind_all("<Button-4>", self.onMouseWheel)
@@ -87,15 +87,13 @@ def collect_files_from_directory(dir, accepts=[".jpg", ".png", ".jpeg"]):
 
   pprint.pprint(hashmap)
   return hashmap
-  
+
 def toggle_image(var,filename):
   # loop through all the buttons to enable or disable each one
   for key, val in refs[to_basename(filename)][DUPLICATES].items():
-    print(key)
-    print(val)
     if val['idx']==var:
       print("flipping ", var)
-      refs[to_basename(filename)][DUPLICATES][key]['save'] = not refs[to_basename(filename)][DUPLICATES][key]['save'] 
+      refs[to_basename(filename)][DUPLICATES][key]['save'] = not refs[to_basename(filename)][DUPLICATES][key]['save']
 
     color = "red"
     if refs[to_basename(filename)][DUPLICATES][key]['save']:
@@ -103,33 +101,35 @@ def toggle_image(var,filename):
 
     refs[to_basename(filename)][DUPLICATES][key]['btn_ref'].config(fg=color)
 
-def advance_pointer(increment=False):
-  available = [x for x in images_mapping if x not in refs or not refs[x][DOOMSDAY_COUNT] > 0]
+def advance_pointer(advance):
   curr_idx = None
-  if len(available) > 0:
-    has_doomsday_at_one = [x for x in images_mapping if x in refs and refs[x][DOOMSDAY_COUNT] == 1]
-    if len(has_doomsday_at_one) > 0:
-      curr_idx = has_doomsday_at_one[-1]
-    else:
-      curr_idx = available[0]
-    print("available: ", curr_idx)
+  available = [x for x in images_mapping if x not in refs or refs[x][DOOMSDAY_COUNT] == 0]
+  if advance:
+    # find first new image
+    print("advancing with ", len(available), "choices")
+    curr_idx = available[0]
   else:
-    has_big_doomsday = [x for x in images_mapping if refs[x][DOOMSDAY_COUNT] > 0]
-    if len(has_big_doomsday) > 0:
-      curr_idx = has_big_doomsday[-1]
-    print("unavailable")
+    prev_seen = [x for x in images_mapping if x in refs and refs[x][DOOMSDAY_COUNT] == 1]
+    if len(prev_seen) > 0:
+      curr_idx = prev_seen[-1]
+    elif len(available) > 0:
+      curr_idx = available[0]
+    print("undoing with ", len(available), "choices")
 
-  if increment:
-    if curr_idx not in refs:
-      refs[curr_idx] = {
-        DOOMSDAY_COUNT: 0,
-        DUPLICATES: {},
-      }
+  if curr_idx and curr_idx not in refs:
+    refs[curr_idx] = {
+      DOOMSDAY_COUNT: 0,
+      DUPLICATES: {},
+    }
+  
+  if(advance):
     refs[curr_idx][DOOMSDAY_COUNT] += 1
 
-
+  print("current index", images_mapping_keys.index(curr_idx))
+  print([refs[x][DOOMSDAY_COUNT] for x in refs if DOOMSDAY_COUNT in refs[x]])
   draw_buttons(curr_idx)
-  pprint.pprint(refs)
+
+
 
 def undo(current_page_idx):
   curr_index = images_mapping_keys.index(current_page_idx)
@@ -137,11 +137,11 @@ def undo(current_page_idx):
   for key in images_mapping_keys:
     if images_mapping_keys.index(key) <= curr_index:
       refs[key][DOOMSDAY_COUNT] -= 1
-
       if refs[key][DOOMSDAY_COUNT] < 0:
         refs[key][DOOMSDAY_COUNT] = 0
 
-  advance_pointer()
+  print("<-- undo")
+  advance_pointer(advance=False)
 
 def advance(current_page_idx):
   # increment doomsday on the doomed baskets
@@ -150,6 +150,8 @@ def advance(current_page_idx):
     curr_index = images_mapping_keys.index(current_page_idx)
   else:
     curr_index = -1
+
+  print("current index", current_page_idx, "|", curr_index )
 
   for key in images_mapping_keys:
     if images_mapping_keys.index(key) <= curr_index:
@@ -163,11 +165,10 @@ def advance(current_page_idx):
             os.remove(filename)
         else:
           keep[filename] = val
-      print("keeeep", keep)
       refs[key][DUPLICATES] = keep
 
-        
-  advance_pointer(increment=True)
+  print("--> advance")
+  advance_pointer(advance=True)
 
 def get_color(id1, id2):
   if id2 in refs[id1][DUPLICATES] and "save" in refs[id1][DUPLICATES][id2] and refs[id1][DUPLICATES][id2]['save']:
@@ -212,9 +213,9 @@ def add_to_memory(current_page_idx):
 
     # create the button
     btn = tk.Button(
-        content_frame, 
+        content_frame,
         image=img,
-        text=image_path, 
+        text=image_path,
           wraplength=180,
         compound=tk.BOTTOM,
         command=lambda var=i, path=image_path:toggle_image(var,path),
@@ -228,16 +229,16 @@ def add_to_memory(current_page_idx):
 def draw_buttons(current_page_idx):
   for widget in content_frame.winfo_children():
     widget.destroy()
-  
+
   undo_button = tk.Button(
-    content_frame, 
+    content_frame,
     text="<- Undo",
     command=lambda: undo(current_page_idx),
   )
   undo_button.grid(row=1,column=1)
 
   advance_button = tk.Button(
-    content_frame, 
+    content_frame,
     text="Advance ->",
     command=lambda: advance(current_page_idx),
   )
@@ -246,12 +247,12 @@ def draw_buttons(current_page_idx):
   if current_page_idx:
     add_to_memory(current_page_idx)
 
-  
-      
+
+
 
 
 # pull images
-images_mapping = collect_files_from_directory('images') 
+images_mapping = collect_files_from_directory('images')
 images_mapping_keys = list(images_mapping.keys())
 
 print("mapping,", images_mapping_keys)
@@ -265,13 +266,13 @@ DUPLICATES = 'duplicates'
 
 
 
-      
-      # Now add some controls to the scrollframe. 
+
+      # Now add some controls to the scrollframe.
       # NOTE: the child controls are added to the view port (scrollFrame.viewPort, NOT scrollframe itself)
 
       # when packing the scrollframe, we pack scrollFrame itself (NOT the viewPort)
       # self.
-  
+
 
 
 
