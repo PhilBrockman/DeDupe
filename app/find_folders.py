fdfc03318406c9f4fc0f75d18715dbf5de3cf662
Phil Brockman <phil.brockman@gmail.com>
2021-12-26 21:29:27 -0600

    Create find_folders.py

---
+++ app/find_folders.py
@@ -0,0 +1,18 @@
+import glob, os, pprint
+
+def collect_files_from_directory(dir):
+  hashmap = {}
+  for f in glob.glob("./**", recursive=True):
+    if os.path.isdir(f):
+      name = os.path.basename(f)
+      if name not in hashmap:
+        hashmap[name] = []
+      hashmap[name].append({
+        "path": f,
+        "count": sum(len(files) for _, _, files in os.walk(f)),
+      })
+
+  return hashmap
+
+foo = collect_files_from_directory(".")
+pprint.pprint(foo)
